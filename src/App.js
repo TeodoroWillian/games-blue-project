da1bbaca704d3a779441901ad90bb16c8af5992b
Willian Teodoro <totawillian@gmail.com>
2023-02-18 13:46:41 -0400

    feat(home): implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Genres from "./pages/Genres/Genres";
 import Genre from "./pages/Genre/Genre";
+import Home from "./pages/Home/Home";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Genres />} />
         <Route path="/genre/:id" element={<Genre />} />
+        <Route path="/home" element={<Home />}/>
       </Routes>
     </>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,15 @@
-import gameFetch from "../../config.json"
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ReactPlayer from "react-player";
 import './Home.css'
+import axios from "axios";
 
 const Home = () => {
   const [games, setGames] = useState([]);
 
   const getGames = async () => {
     try {
-      const response = await gameFetch.get("/game");
+      const response = await axios.get("http://localhost:3333/game");
 
       const data = response.data;
 
@@ -36,14 +36,14 @@ const Home = () => {
             <div className="games">
               <h2>{game.title}</h2>
               <p>{game.description}</p>
-              <Link to={`/games/${game.id}`} className="btn">
+              <Link to={`/games/${game.id}`} className="btn-1">
                 Ler mais
               </Link>
             </div>
             <div className="videos">
               <h2>Trailer:</h2>
               <ReactPlayer url={game.trailerYoutubeGamePlay} />
-              <h2>Gamplay:</h2>
+              <h2>Gameplay:</h2>
               <ReactPlayer url={game.gamePlayYoutubeUrl} />
             </div>
           </div>
